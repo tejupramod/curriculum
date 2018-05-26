0803dbd7eb7e7a5270009754d7b59e8939522c3d
Teju <teju@garagescript.org>
2018-05-26 16:25:52 -0700

    larger than 5

--- curriculum/js2/6.js
+++ curriculum/js2/6.js
@@ -6,6 +6,14 @@
 
 const solution = (a) => {
   return [];
+  const solution =(a,array=[],i=0){
+    if(a.length===0)
+      return [];
+    if(a.length===i)
+      return array;
+    if(a[i]>5)
+      array.push(a[i]);
+    return solution(a,array.i+1);
 };
 
 module.exports = {
