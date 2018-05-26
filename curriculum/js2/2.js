c22d8aea3d1a8c18b296e4328c6c2f92dc74489c
Teju <teju@garagescript.org>
2018-05-26 15:36:16 -0700

    odd elements changed to 0

--- curriculum/js2/2.js
+++ curriculum/js2/2.js
@@ -1,4 +1,5 @@
-/**
+
+`/**
  * Given an array, return the same array
  *   where all odd elements are changed to 0
  * @param {array} a
@@ -7,6 +8,18 @@
 
 const solution = (a) => {
   return [];
+  const solution = (arr,new arr=[],i=0)=>{
+    if(arr.length===i){
+      return new arr;
+    }
+    if(arr[i]%2===0){
+      return newarr.push(0);
+    }
+    else {
+      return arr;
+    }
+    retrun solution(arr,newarr,i+1);
+  }
 };
 
 module.exports = {
