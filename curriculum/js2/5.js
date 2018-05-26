15af1ca3e0cb67cfabf08c32cb7e0c7a5dddad2c
Teju <teju@garagescript.org>
2018-05-26 16:20:59 -0700

    largest element

--- curriculum/js2/5.js
+++ curriculum/js2/5.js
@@ -5,7 +5,13 @@
  */
 
 const solution = (a) => {
-  return 0;
+  return o
+  
+      +const solution = (a, i=0, max = null) => {
+        +  if(a.length===0) return '';
+        +  if(i === a.length) return max;
+        +  if(max === null||a[i] > max) max = a[i];
+        return solution(a,i+1,max);
 };
 
 module.exports = {
