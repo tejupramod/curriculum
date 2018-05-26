1db62e8e848fcab7be818e579530a0bc23c79d81
Teju <teju@garagescript.org>
2018-05-26 16:11:41 -0700

    prime

--- curriculum/js2/3.js
+++ curriculum/js2/3.js
@@ -8,6 +8,23 @@
 
 const solution = (a) => {
   return [];
+  const isPrime =(a,i=0)=>{
+    if(a===1)
+      return false;
+    if(i===a)
+      return true;
+    if(a%i===0)
+      return false;
+    const solution=(a=[],array=[],i)=>{
+      if(i===a.length)
+        return array;
+      if(!isPrime(a[i])
+        array.push(1);
+        else
+        array.psuh(a[i]);
+        return solution(a,array,i+1)
+
+
 };
 
 module.exports = {
