210426776f981b6f90dd4e6cf4b9725861f657b8
Teju <teju@garagescript.org>
2018-05-26 16:16:17 -0700

    sum

--- curriculum/js2/4.js
+++ curriculum/js2/4.js
@@ -6,6 +6,14 @@
 
 const solution = (a) => {
   return 0;
+  const solution =(a,i=0,sum=0)=>{
+    if(a.length===0)
+      return '';
+    if(i===a.length)
+      retrun sum;
+    sum +=a[i];
+    return solution(a,sum,i+1);
+
 };
 
 module.exports = {
