81ba80a835199eb2c63c601245d8adf8d12a9b8e
Teju <teju@garagescript.org>
2018-06-02 15:07:37 -0700

    matched elements

--- curriculum/js2/7.js
+++ curriculum/js2/7.js
@@ -8,6 +8,14 @@
 
 const solution = (a, b) => {
   return 0;
+    const solution =(a,b,i=0,count=0)=>{
+      if(a.length===i){
+        return count;
+      }
+      if(a[i]===b){
+        count++;
+      }
+      return solution(a,b,i+1,count);
 };
 
 module.exports = {
