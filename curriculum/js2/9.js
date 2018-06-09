933581379acff8cfbe528e0db1eda13044e9ef8e
Teju <teju@garagescript.org>
2018-06-09 15:02:24 -0700

    mapfunction

--- curriculum/js2/9.js
+++ curriculum/js2/9.js
@@ -4,10 +4,12 @@
  */
 
 const solution = () => {
-  Array.prototype.gsReduce = function () {
-    // To get the actual array, use this
-    return [];
-  };
+  Array.prototype.gsReduce = function (func,newarr =[],i=0) {
+    if (this.length ===i)
+      return newarr;
+    newarr.push(func(this[i]));
+    return this.gsMap(func,newarr,i+1);
+  }
 };
 
 module.exports = {
